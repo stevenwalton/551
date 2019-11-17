676a3c33dbf8d287b35eb170b1298e21b81e245e
Steven Walton <swalton2@cs.uoregon.edu>
2019-11-16 17:16:12 -0800

    Some minor formatting changes. Let's try on ix now

--- assignment4/getCust.php
+++ assignment4/getCust.php
@@ -1,8 +1,33 @@
 <?php
 include_once 'connect.php';
 include_once 'databases.php';
-
-$manu = $_POST['manu'];
-$object = new Databases;
-$object->getCustomers($manu);
 ?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Results From Query</title>
+    </head>
+    <body>
+    
+    <?php
+    $manu = $_POST['manu'];
+    echo("Querying customers for: ".$manu);
+    echo("<br>");
+    echo("<br>");
+    $object = new Databases;
+    $object->getCustomers($manu);
+    ?>
+    </body>
+    <footer><font size="1">
+        This webpage is for 
+        <a href="https://classes.cs.uoregon.edu/19F/cis451/assts/asst4/asst4.html">
+        Assignment 4</a> from UO's CIS 551 (Database Processing).
+        <br>
+        Year: 2019
+        <br>
+        Work performed by: Steven Walton
+    </font>
+    </footer>
+</html>
