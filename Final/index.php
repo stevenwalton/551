f6ca25c5cbda01f4084824ca0370b889db9a0465
Steven Walton <swalton2@cs.uoregon.edu>
2019-12-01 16:10:16 -0800

    Setup
    Created an index page with the title and picture. Initialized the media
    folder to hold all the pictures. This will be with git-lfs.

---
+++ Final/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Climbing Project</title>
+    </head>
+    <body>
+    <img src="./media/Title.png">
+    <br>
+    Welcome to Climbing Project. Here you may explore and search for different 
+    climbing routes. Remember to climb safe!
+    <br>
+    <!--
+    <form action="getCust.php" method="POST">
+    <input type="text" name="manu"> 
+    <input type="submit" value="submit">
+    -->
+    </form>
+    <br>
+    </body>
+
+    <footer><font size="1">
+        This webpage is the final project for
+        <a href="https://classes.cs.uoregon.edu/19F/cis451/">
+        UO's CIS 551 (Database Processing)</a>.
+        <br>
+        Year: 2019
+        <br>
+        Work performed by: Steven Walton
+        <br>
+        This project was inspired by <a href="https://www.mountainproject.com/">
+        Mountain Project</a>. Some photos may be taken from there. This project 
+        is not intended for commercial use.
+    </font>
+    </footer>
+</html>
